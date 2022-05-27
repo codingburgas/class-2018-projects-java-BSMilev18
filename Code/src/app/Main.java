46e21f69838e59ffccc82d15312c3681f2af75a3
BSMilev18@codingburgas.bg <BSMilev18@codingburgas.bg>
2022-05-27 15:50:04 +0300

    Added initial packages

---
+++ Code/src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+import controllers.UserController;
+import database.UserDatabase;
+import services.UserService;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
