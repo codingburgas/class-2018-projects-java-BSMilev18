416fdfe5114ebabb53629e478a56b819974f1aed
edkehayova18 <58166338+edkehayova18@users.noreply.github.com>
2022-06-14 17:58:33 +0300

    Add contactAndHelpMenu class

---
+++ Code/src/app/contactAndHelpMenu.java
@@ -0,0 +1,18 @@
+package contactAndHelpMenu;
+
+public class contactAndHelpMenu {
+
+	public static void contactMain(String[] args) {
+		System.out.println("============================");
+		System.out.println("|      CONTACT & HELP      |");
+		System.out.println("============================");
+		System.out.println("|       Please, choose     |");
+		System.out.println("|     an option:           |");
+		System.out.println("|--------------------------|");
+		System.out.println("|   1. Contact support     |");
+		System.out.println("|   2. Open manual         |");
+		System.out.println("============================");
+
+	}
+
+}
